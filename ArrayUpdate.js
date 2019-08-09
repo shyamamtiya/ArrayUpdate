1adcab555b9db30088ca9d5503eedec20e3191ff
shyam <shyam.amratiya@vedantu.com>
2019-08-09 17:29:23 +0530

    filter

--- ArrayUpdate.js
+++ ArrayUpdate.js
@@ -9,3 +9,9 @@ let a= [{'abc':1},{'abc':2},{'abc':3}];
 let index =1;
 c =[ ...a.slice(0,index),{...a[index],'abc':4},...a.slice(index)  ];
 //output : - [{'abc':1},{'abc':4},{'abc':3}]
+
+
+//Remove element from array in immutable way
+
+let array = [1,2,3,4,5];
+let newArray = array.filter(el => el > 3);
